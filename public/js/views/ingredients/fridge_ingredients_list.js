2600e75eb001264abcb37def4b5393b343e8bf4c
MSeaman <Matthew@Matthews-MacBook-Air.local>
2015-04-01 15:11:45 -0400

    more fridge view changes

--- public/js/views/ingredients/fridge_ingredients_list.js
+++ public/js/views/ingredients/fridge_ingredients_list.js
@@ -1,6 +1,18 @@
 App.Views.FridgeIngredientList = Backbone.View.extend({
 
+	el: ,
 
+  initialize: function() {
+    this.listenTo(this.collection, 'reset', this.renderAll);
+  },
+
+  renderAll: function() {
+    this.collection.each(this.renderOne, this)
+  },
+
+  renderOne: function(house) {
+    this.$el.append(new App.Views.FridgeIngredient({model: fridge_ingredient}).$el);
+  }
 
 	
 })
