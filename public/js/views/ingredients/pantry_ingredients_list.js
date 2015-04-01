c11cc1e70872078a8ff013fbf7f760891ee9da47
MSeaman <Matthew@Matthews-MacBook-Air.local>
2015-04-01 17:48:11 -0400

    added pantry/search stuff

---
+++ public/js/views/ingredients/pantry_ingredients_list.js
@@ -0,0 +1,18 @@
+App.Views.PantryIngredientList = Backbone.View.extend({
+
+  
+
+  initialize: function() {
+    this.listenTo(this.collection, 'reset', this.renderAll);
+  },
+
+  renderAll: function() {
+    this.collection.each(this.renderOne, this)
+  },
+
+  renderOne: function(house) {
+    this.$el.append(new App.Views.PantryIngredient({model: pantry_ingredient}).$el);
+  }
+
+  
+})
