ebc6bc69231034fc0913f3575d9817fc22afbf8e
Andrew Carton <andrew.carton1@gmail.com>
2015-04-02 18:06:02 -0400

    asd

--- public/js/views/ingredients/fridge_ingredients_list.js
+++ public/js/views/ingredients/fridge_ingredients_list.js
@@ -13,37 +13,34 @@ App.Views.FridgeIngredientList = Backbone.View.extend({
     'click .add-to-search': 'selectIngredient',
     'click .delete': 'delete',
     'click .add-ingredient': 'addIngredient',
-    'click .add-to-pantry': 'addToPantry',
+    'click .add-to-pantry': 'moveIngredientToPantry',
     'click .select-user':'setUser',
   },
 
   setUser: function() {
     App.fridgeIngredients.userId = $('#userDropDown').val();
-    //App.pantryIngredients.userId = $('#userDropDown').val();
+    App.pantryIngredients.userId = $('#userDropDown').val();
     App.fridgeIngredients.getFridgeIngredients();
-    //App.pantryIngredients.getPantryIngredients();
+    App.pantryIngredients.getPantryIngredients();
   },
 
   addIngredient: function(){
     console.log('add ingredient button clicked brough');
     var ingredientName = $('#add-fridge-ingredient-input').val();
-    //this.collction.userId = $('#userDropDown').val();
     this.collection.create({name: ingredientName});
-    debugger
-    //this.collection.sync();
-
     },
 
-    addToPantry: function(){
-      console.log('add to pantry button clicked');
-      var ingredientArray = [];
-      var singleIng = this.model.get('name');
-      var checkedIng = this.model.get('#fridge-ingredient-checkbox');
-      if (checkedIng.checked =true){
-      ingredientArray.push(singleIng);
-      } else {console.log('poop')};
-      ingredientArray.appendTo('#pantry-ingredients-containter');
-     },
+   moveIngredientToPantry: function () {
+      console.log('select ingredient button clicked doooooood');
+      var fridge = $('#fridge-ingredients-list-container');
+      for (var i = 0; i < fridge.children().length; i ++) {
+        if (fridge.children().eq(i).children().eq(0).children().eq(0).is(':checked') == true) {
+          var ingredientId = parseInt(fridge.children().eq(i).children().eq(0).attr('data-id'));
+          var movedIngredient = App.fridgeIngredients.findWhere({fridgeIngId: ingredientId});
+          App.pantryIngredients.add(movedIngredient);
+        };
+      };
+    },
 
 
     selectIngredient: function () {
