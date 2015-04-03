659fc3ef9a51a8459ad01afdba27ee0f44bca255
MSeaman <Matthew@Matthews-MacBook-Air.local>
2015-04-03 14:51:29 -0400

    fixed bugs

--- public/js/collections/search_ingredients.js
+++ public/js/collections/search_ingredients.js
@@ -1,6 +1,7 @@
  App.Collections.SearchIngredients = Backbone.Collection.extend({
 
 	model: App.Models.FridgeIngredient,
+	
 
 	addModel: function (ingredient) {
 		this.collection.add(ingredient);
