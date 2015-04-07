9a56642d31f81170c8cd74154eca40e6a63cf018
MSeaman <Matthew@Matthews-MacBook-Air.local>
2015-04-07 13:20:38 -0400

    fixed merge

--- public/js/views/searches/search.js
+++ public/js/views/searches/search.js
@@ -5,12 +5,15 @@ App.Views.Search = Backbone.View.extend({
   events: {
     'click .search-button': 'search',
     'click .advanced-search-button': 'advancedSearch',
-    'click .reset-search-button': 'reset'
+    'click .reset-search-button': 'reset',
+    'click .remove-from-search-button': 'removeFromSearch'
   },
 
   initialize: function(){
     this.listenTo(this.collection, 'add', this.renderAll);
     $('#advanced-search-container').hide();
+    $('#recipe-results-container').hide();
+
   },
  
   renderAll: function() {
@@ -24,6 +27,7 @@ App.Views.Search = Backbone.View.extend({
   },
 
   search: function () {
+    $('#recipe-results-container').show();
     App.recipes.search();
     
   },
@@ -34,13 +38,28 @@ App.Views.Search = Backbone.View.extend({
       App.searchIngredients.remove(ingredient);
       $('#search-ingredients-container').empty();
       $('#recipe-results-container').empty();
+      $('#recipe-results-container').hide();
     });
   },
+
+  // removeFromSearch: function () {
+  //   // debugger
+  //   console.log('remove from search button click');
+  //   var search = $('#search-ingredients-container');
+  //   for (var i = 0; i < search.children().length; i++) {
+  //     if (search.children().eq(i).children().children().is(':checked') === true) {
+  //       var ingredientId = parseInt(search.children().eq(i).children().attr('data-id'));
+  //       var movedIngredient = App.searchIngredients.findWhere({fridgeIngId: ingredientId});
+  //       this.collection.remove(movedIngredient);
+  //     }
+  //   }
+  // },
 
   advancedSearch: function(){
 
     //clicks advanced search button and new advanced search view is created and rendered
     var advancedSearchView =  new App.Views.AdvancedSearch;
+    $('#advanced-search-container').show();
 
   }
 
