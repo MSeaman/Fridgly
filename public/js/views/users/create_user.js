a5c4f01aa65b44206ba899071405b9a048a09e07
MSeaman <Matthew@Matthews-MacBook-Air.local>
2015-09-27 17:01:28 -0400

    css fix

--- public/js/views/users/create_user.js
+++ public/js/views/users/create_user.js
@@ -1,38 +1,36 @@
-// App.Views.CreateUser = Backbone.View.extend ({
+App.Views.CreateUser = Backbone.View.extend ({
 
-//   events: {
-//     'click .create-user': 'createUser'
-//   },
+  events: {
+    'click .create-user': 'createUser'
+  },
 
-//   initialize: function() {
-//       this.template = Handlebars.compile($('#users-template').html());
-//   },
+  initialize: function() {
+      this.template = Handlebars.compile($('#users-template').html());
+  },
+
+  el: 'body',
   
-//   getAllergens: function () {
-//     $.ajax({
-//       url: '/users',
-//       method: 'get'
-//     }).done(this.populateUsers)
-//   },
+  getAllergens: function () {
+    $.ajax({
+      url: '/users',
+      method: 'get'
+    }).done(this.populateUsers)
+  },
 
-//   populateAllergens: function (users) {
-//     for (var i = 0; i < users.length; i++) {
-//       var allergenName = users[i].name;
-//       var userId = users[i].id;
-//       var dropDownItem = $('<option>').attr('value', userId).html(userName);
-//       $('#userDropDown').append(dropDownItem);
-//     };
-//   },
+  populateAllergens: function (users) {
+    for (var i = 0; i < users.length; i++) {
+      var allergenName = users[i].name;
+      var userId = users[i].id;
+      var dropDownItem = $('<option>').attr('value', userId).html(userName);
+      $('#userDropDown').append(dropDownItem);
+    };
+  },
 
-//   createUser: function () {
-//     $.ajax({
-//       url: '/users',
-//       method: 'POST',
-//       data: {
-//         name: $('#create-user-input').val(),
-//         allergy: 
-//       }
-//     })
-//   }
+  createUser: function () {
+    console.log('create user button clicked brough');
+    var userName = $('#create-user-input').val();
+    this.collection.create({name: userName});
 
-// })
+  }
+
+});
